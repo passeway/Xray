311da8eee9af5e8942ddd3c3908d57ab90d2fcc7
passeway <164740303+passeway@users.noreply.github.com>
2024-04-16 07:11:50 +0800

    更新 reality.sh

--- reality.sh
+++ reality.sh
@@ -14,10 +14,10 @@ uninstall_vless_reality() {
 echo "请选择操作:"
 echo "1. 安装 vless-reality"
 echo "2. 卸载 vless-reality"
-read -p "请输入选项编号: " choice
+read -rp "请输入选项编号: " choice
 
 # 根据用户选择执行相应操作
-case $choice in
+case "$choice" in
     1) install_vless_reality ;;
     2) uninstall_vless_reality ;;
     *) echo "无效的选项" ;;
