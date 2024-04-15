74377be31f691754e8e37536260713322e459201
passeway <164740303+passeway@users.noreply.github.com>
2024-04-16 07:35:11 +0800

    更新 reality.sh

--- reality.sh
+++ reality.sh
@@ -1,3 +1,21 @@
+#!/bin/bash
+
+# 函数用于安装 vless-reality
+install_vless_reality() {
+    curl -sS -o vless-reality.sh https://raw.githubusercontent.com/passeway/reality/main/vless-reality.sh && chmod +x vless-reality.sh && ./vless-reality.sh
+}
+
+# 函数用于卸载 vless-reality
+uninstall_vless_reality() {
+    bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ remove --purge
+}
+
+# 显示选项菜单
+echo "请选择操作:"
+echo "1. 安装 vless-reality"
+echo "2. 卸载 vless-reality"
+read -rp "请输入选项编号: " choice
+
 # 根据用户选择执行相应操作
 case "$choice" in
     1) install_vless_reality ;;
