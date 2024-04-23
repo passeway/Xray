61d662850c121579d34be8949ef150461ab9d547
passeway <164740303+passeway@users.noreply.github.com>
2024-04-23 19:38:14 +0800

    Update vless-reality.sh

--- vless-reality.sh
+++ vless-reality.sh
@@ -106,45 +106,9 @@ EOF
     systemctl enable xray.service && systemctl restart xray.service
     rm -f tcp-wss.sh install-release.sh reality.sh
 
-    cat >/usr/local/etc/xray/reclient.json <<EOF
-{
-===========配置参数=============
-代理模式：vless
-地址：$(getIP)
-端口：${PORT}
-UUID：${v2uuid}
-流控：xtls-rprx-vision
-传输协议：tcp
-Public key：${rePublicKey}
-底层传输：reality
-SNI: www.apple.com
-shortIds: 88
-====================================
-vless://${v2uuid}@$(getIP):${PORT}?encryption=none&flow=xtls-rprx-vision&security=reality&sni=www.amazon.com&fp=chrome&pbk=${rePublicKey}&sid=88&type=tcp&headerType=none#$IP_COUNTRY
-
-}
-EOF
-
-    clear
-}
-
-client_re() {
-    echo
     echo "安装已经完成"
-    echo "===========reality配置参数============"
-    echo "代理模式：vless"
-    echo "地址：$(getIP)"
-    echo "端口：${PORT}"
-    echo "UUID：${v2uuid}"
-    echo "流控：xtls-rprx-vision"
-    echo "传输协议：tcp"
-    echo "Public key：${rePublicKey}"
-    echo "底层传输：reality"
-    echo "SNI: www.apple.com"
-    echo "shortIds: 88"
-    echo "===================================="
-    echo "vless://${v2uuid}@$(getIP):${PORT}?encryption=none&flow=xtls-rprx-vision&security=reality&sni=www.apple.com&fp=chrome&pbk=${rePublicKey}&sid=88&type=tcp&headerType=none#$IP_COUNTRY"
-    echo
+    echo "vless://${v2uuid}@$(getIP):${PORT}?encryption=none&flow=xtls-rprx-vision&security=reality&fp=chrome&pbk=${rePublicKey}&sid=88&type=tcp&headerType=none#$IP_COUNTRY"
+
 }
 
 install_xray
