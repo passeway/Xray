65a16b3c3c68b2895d15b1003843adedac72e6ec
passeway <164740303+passeway@users.noreply.github.com>
2024-10-22 20:32:33 +0800

    Update xray.sh

--- xray.sh
+++ xray.sh
@@ -235,11 +235,11 @@ EOF
     
     echo "Xray 安装成功"
     echo ""
-    echo "${IP_COUNTRY} = ss, ${HOST_IP}, ${sport}, encrypt-method=2022-blake3-aes-128-gcm, password=${psk}, udp-relay=true"
+    echo "${IP_COUNTRY} = ss, ${HOST_IP}, ${PORT2}, encrypt-method=2022-blake3-aes-128-gcm, password=${psk}, udp-relay=true"
     echo ""
     echo "${IP_COUNTRY} = vmess, example.com, 8880, username=${v2uuid}, ws=true, ws-path=/?ed=2560, ws-headers=Host:\"example.com\", vmess-aead=true"
     echo ""
-    echo "vless://${v2uuid}@${HOST_IP}:${PORT}?encryption=none&flow=xtls-rprx-vision&security=reality&sni=www.tesla.com&fp=chrome&pbk=${rePublicKey}&sid=123abc&type=tcp&headerType=none#${IP_COUNTRY}"
+    echo "vless://${v2uuid}@${HOST_IP}:${PORT1}?encryption=none&flow=xtls-rprx-vision&security=reality&sni=www.tesla.com&fp=chrome&pbk=${rePublicKey}&sid=123abc&type=tcp&headerType=none#${IP_COUNTRY}"
 }
 
 install_xray
